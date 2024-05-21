d41877458f37e44f53666a7236096856b8f21092
asfandyar khan <Fandy@Fandys-Macbook-pro.local>
2024-05-21 14:12:53 +0800

    Fix ESLint configuration and version issues

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ module.exports = [
   {
     files: ["*.js"],
     languageOptions: {
-      ecmaVersion: 12,
+      ecmaVersion: 2021,
       sourceType: "module",
       globals: {
         browser: true,
